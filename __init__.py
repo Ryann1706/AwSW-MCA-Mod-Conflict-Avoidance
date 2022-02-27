3e9bce1e800b183cd0e9755d3e6ea9593d03fa16
Ryann1706 <91089632+Ryann1706@users.noreply.github.com>
2022-02-27 17:03:07 +0000

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,22 @@
+
+from modloader import modast, modinfo
+from modloader.modclass import Mod, loadable_mod
+import jz_magmalink as ml
+
+@loadable_mod
+class AWSWMod(Mod):
+    name = "Mod Conflict Avoidance"
+    version = "0.0"
+    author = "Ryann1706"
+    dependencies = ["MagmaLink"]
+
+    def mod_load(self):
+        pass
+
+    def mod_complete(self):
+        pass
+
+
+    ml.find_label("adinegoodending") \
+        .search_say("I don't know.", depth=400) \
+        .hook_to("ryann_mca_adine_good_ending")
